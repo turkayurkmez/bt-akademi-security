a264ad40b0254bcc1d61cb224b49af2729bc76d4
türkay ürkmez <turkay.urkmez@outlook.com>
2024-07-03 15:58:09 +0300

    Secure Http Response

---
+++ SecureResponseHeader/SecureResponseHeader/Program.cs
@@ -0,0 +1,37 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.Use(async (context, next) =>
+{
+    if (!context.Response.Headers.ContainsKey("X-Frame-Options"))
+    {
+        context.Response.Headers.Add("X-Frame-Options", "DENY");
+    }
+    await next();
+});
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
